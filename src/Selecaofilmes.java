b5e0e134d56aa7a5ff533fea1df80e8b4dd089d4
Wedson Wagner da Silva Filho <wedsonwagner007@gmail.com>
2025-04-28 01:16:36 -0300

    confirmacao do filme

--- src/Selecaofilmes.java
+++ src/Selecaofilmes.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Selecaofilmes {
     public static void main(String[] args) {
-        Scanner imput = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         String[] filmes = {
                 "1. Um Filme Minecraft",
@@ -14,12 +15,24 @@ public class Selecaofilmes {
                 "7. Capitão América: Admirável Mundo Novo",
                 "8. Flow"
         };
-        System.out.println("Filmes:");
+        System.out.println("========== Filmes: ===========");
         for (String filme : filmes) {
             System.out.println(filme);
         }
-        System.out.println("Escolha um filme:");
-        String escolha = imput.nextLine();
+        System.out.println("===== Escolha um filme: =====");
+        int escolha = input.nextInt();
+        System.out.println("🎬 Filme escolhido:");
+        if (escolha >= 1 && escolha <= filmes.length) {
+            System.out.println("==> " + filmes[escolha - 1].substring(3));
+        } else{
+            System.out.println(" ⚠\uFE0F Filme fora de cartaz");
+        }
+        System.out.println("===============================\n");
+
+
+
+
+
         }
     }
 
